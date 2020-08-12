7fed18b7c0fb458a601b1305d89b61807f7fce16
10yeony <10yeony@gmail.com>
2020-08-12 12:46:36 +0900

    edit SpringTest

---
+++ spring/springWorkspace/sp14_RestfulAPI/src/main/java/com/encore/rest/domain/Person.java
@@ -0,0 +1,31 @@
+package com.encore.rest.domain;
+
+public class Person {
+	private String name;
+	private String message;
+	public Person(String name, String message) {
+		super();
+		this.name = name;
+		this.message = message;
+	}
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", message=" + message + "]";
+	}	
+}
