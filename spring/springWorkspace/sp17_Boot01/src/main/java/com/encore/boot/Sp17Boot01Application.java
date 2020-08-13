4c3dcb5885501163448c3b3a256b7ed730d9ea80
10yeony <10yeony@gmail.com>
2020-08-13 16:53:46 +0900

    [2020-08-13 4:53 p.m.] add Spring Boot

---
+++ spring/springWorkspace/sp17_Boot01/src/main/java/com/encore/boot/Sp17Boot01Application.java
@@ -0,0 +1,15 @@
+package com.encore.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//이 파일은 부트 파일 실행시킴
+//이거 돌리면 Spring Boot가 돌아감
+@SpringBootApplication
+public class Sp17Boot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp17Boot01Application.class, args);
+	}
+
+}
